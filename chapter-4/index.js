b0ff2c12c0b0015ce04bc581230debd0c0cee128
Rukshona Rad <rukshona.rad@gmail.com>
2023-10-17 21:52:11 -0400

    Start problem

--- chapter-4/index.js
+++ chapter-4/index.js
@@ -21,3 +21,10 @@ function getDigitsFromString(str) {
 const inputString = "0s1a3y5w7h9a2t4?6!8?0";
 const result = getDigitsFromString(inputString);
 console.log(result);
+
+// Acronyms Create a function that, given a string, returns the string’s acronym (first letters only, capitalized). Given " there's no free lunch  -  gotta pay yer way. ", return "TNFL-GPYW". Given "Live from New York, it's Saturday Night!", return "LFNYISN".
+
+const acronyms = (str) => {
+    return str.split(" ");
+};
+console.log(acronyms(" there's no free lunch -  gotta pay yer way. "));
