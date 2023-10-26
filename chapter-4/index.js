d6081d0159f91d4b7e7a9ac223cb14b29fbd9fa1
Rukshona Rad <rukshona.rad@gmail.com>
2023-10-25 22:24:36 -0400

    Solve array zip

--- chapter-4/index.js
+++ chapter-4/index.js
@@ -158,3 +158,19 @@ console.log(
         "incarceration"
     ])
 );
+
+//  Zip Arrays into Map Associative arrays are sometimes called maps because a key (string) maps to a value. Given two arrays, create an associative array (map) containing keys of the first, and values of the second. For arr1 = ["abc", 3, "yo"] and arr2 = [42, "wassup", true], return {"abc": 42, 3: "wassup", "yo": true}.
+const zipArrays = (arr1, arr2) => {
+    const result = {};
+
+    for (let i = 0; i < arr1.length; i++) {
+        result[arr1[i]] = arr2[i];
+    }
+
+    return result;
+};
+
+const arr1 = ["abc", 3, "yo"];
+const arr2 = [42, "wassup", true];
+const zippedMap = zipArrays(arr2, arr1);
+console.log(zippedMap);
