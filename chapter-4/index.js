fbd56351d5ec7df2350215137bba15d3849bbe68
Rukshona Rad <rukshona.rad@gmail.com>
2023-10-28 22:29:18 -0400

    Count object key without lenghth proparty

--- chapter-4/index.js
+++ chapter-4/index.js
@@ -188,3 +188,22 @@ function invertHash(assocArr) {
 }
 
 console.log(invertHash({ name: "Zaphod", charm: "high", morals: "dicey" }));
+
+//  Associative Array: Number of Values (without .Length) Without using the .length property that is present on all arrays, determine and return the number of values in the given array. If we were to do this on a numerical array, we might check to see whether the element at a certain numerical index was undefined. Unfortunately, we can’t do that here because the keys don’t have any sort of predictable order or first value. So, for object { band: "Travis Shredd & the Good Ol’ Homeboys", style: "Country/Metal/Rap", album: "668: The Neighbor of the Beast" }, you should return the value 3, because there are three keys in this object: band, style and album.
+
+const showArraysKey = (assocArr) => {
+    let result = 0;
+    for (const key in assocArr) {
+        if (assocArr[key]) {
+            result++;
+        }
+    }
+    return result;
+};
+console.log(
+    showArraysKey({
+        band: "Travis Shredd & the Good Ol’ Homeboys",
+        style: "Country/Metal/Rap",
+        album: "668: The Neighbor of the Beast"
+    })
+);
