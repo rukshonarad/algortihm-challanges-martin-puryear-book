a552a14822a5348e1440286a158fea26d2082d38
Rukshona Rad <rukshona.rad@gmail.com>
2023-11-09 22:32:38 -0500

    Nested array

--- chapter-3/index.js
+++ chapter-3/index.js
@@ -280,3 +280,23 @@ const combineTwoArray = (arr1, arr2) => {
 };
 
 console.log(combineTwoArray([1, 2], [10, 20, 30, 40]));
+
+// / Challenge Description:
+// Write a JavaScript function that takes an array as input. This array can contain elements that are either numbers or arrays of numbers, but the arrays will not contain further nested arrays. The function should return a new array that is a flattened version of the input array, where all elements are at the root level.
+
+// [1,2,3[4,5,6[7,8,9]]]=>[1,2,3,4,5,6]
+const flattenArray = (arr) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const ele = arr[i];
+        if (Array.isArray(ele)) {
+            for (let j = 0; j < ele.length; j++) {
+                newArr.push(arr[i][j]);
+            }
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+};
+flattenArray([1, 2, 3, [4, 5, 6], [7, 8, 9]]);
